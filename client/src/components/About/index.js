bf4bcb3f704f36fdf863f500ebaaa017d6673935
Ervin Natareno <ean1207@gmail.com>
2024-04-25 17:09:14 -0700

    Added About page content

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,7 +3,32 @@ import React from 'react'
 export default function About() {
     return (
         <>
-            <div>Hello World!</div>
+            <section className='text-white h-full lg:w-1/2 w-4/5 m-auto font-oxygen'>
+                <div className='flex flex-col justify-center text-center h-full gap-8'>
+                    <h1 className='text-5xl font-semibold'>
+                        About Gainz
+                    </h1>
+                    <p className='text-lg tracking-widest'>
+                        Gainz is more than just a workout app - it's a comprehensive fitness
+                        companion designed to help you achieve your goals and track your progress
+                        along the way. The idea behind Gainz stemmed from the need to have a
+                        centralized platform where fitness enthusiasts can conveniently log and
+                        manage their workouts, track personal bests, and plan their training
+                        sessions effectively. With Gainz, you can easily keep all your workouts in
+                        one place and update them whenever you surpass your previous achievements.
+                        Whether you're lifting weights, running, cycling, or engaging in any other
+                        form of exercise, Gainz allows you to monitor your progress and stay
+                        motivated to push yourself further. In addition to its workout tracking
+                        features, Gainz also serves as a social platform where users can share
+                        their fitness journeys, post updates about their workouts, and interact
+                        with other members of the community. Whether you're looking for
+                        inspiration, support, or simply want to connect with like-minded
+                        individuals, Gainz provides a space where you can share your experiences
+                        and celebrate your achievements together. Join the Gainz community today
+                        and take your fitness journey to the next level!
+                    </p>
+                </div>
+            </section>
         </>
     )
 }
